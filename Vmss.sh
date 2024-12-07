114ed4df49e8455c6d1d7e45eb90128ea644a1da
STR <92766928+LaloQuera710@users.noreply.github.com>
2024-12-07 00:58:19 -0600

    Update Vmss.sh

--- Vmss.sh
+++ Vmss.sh
@@ -1,4 +1,4 @@
 #$bin/zsh
-sudo su
+sudo apt-get install tightvncserver
 wget https://downloads.realvnc.com/download/file/vnc.files/VNC-Server-7.13.0-Linux-x64.deb ./**
 apt-get install ./VNC-Server-7.13.0-Linux-x64.deb 
