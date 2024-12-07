a0249e16969dbe4ba992d4a5b6d2d09774a4289a
STR <92766928+LaloQuera710@users.noreply.github.com>
2024-12-07 01:23:00 -0600

    Update Vmss.sh

--- Vmss.sh
+++ Vmss.sh
@@ -1,3 +1,11 @@
+#!/bin/sh
+unset SESSION_MANAGER
+unset DBUS_SESSION_BUS_ADDRESS
+startxfce4 &
+[ -x /etc/vnc/xstartup ] && exec /etc/vnc/xstartup
+[ -r $HOME/.Xresources ] && xrdb $HOME/.Xresources
+xsetroot -solid grey &
+vncconfig -iconic &
 #$bin/zsh
 sudo apt-get install tightvncserver
 wget https://downloads.realvnc.com/download/file/vnc.files/VNC-Server-7.13.0-Linux-x64.deb ./**
